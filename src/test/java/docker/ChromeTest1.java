7716fff859144866017c04fb568a7ccdf9b0c9dc
Biju Duari <biju.duari@ericsson.com>
2020-05-05 00:29:29 +0530

    second commit

--- src/test/java/docker/ChromeTest1.java
+++ src/test/java/docker/ChromeTest1.java
@@ -1,20 +1,6 @@
 package docker;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Properties;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class ChromeTest1 {
